93fe7b962dedb56637df6989f06a219c2ede331c
Nate Nolting <naten@paulbunyan.net>
2022-05-26 18:45:39 -0500

    220525 | wip | fxhash

--- fxhash/220525/src/index.js
+++ fxhash/220525/src/index.js
@@ -7,7 +7,7 @@ const SMALL = 'tight';
 const MEDUIM = 'middle';
 const LARGE = 'loose';
 const X_LARGE = 'loosest';
-const PX_DENSITY = 4;
+const PX_DENSITY = 1;
 const NOISE_SEED = (fxrand() * 100_000) >> 0
 const PALLET_ID = Math.floor(fxrand() * 4200);
 const CANVAS_WIDTH_MULTIPLIER = 1
@@ -15,7 +15,13 @@ const CANVAS_HEIGHT_MULTIPLIER = 1
 const SIDE_HEIGHT_MULTIPLIER = 28 / 49;
 const TRANSPARENT = false;
 const DEBUG = true;
-const BASE_CELLS_PER_SIDE = 9;
+const BASE_CELLS_PER_SIDE = 15;
+const BASE_CANVAS_WIDTH = 800;
+const BASE_CANVAS_HEIGHT = 800;
+const ENLARGED_CANVAS_WIDTH = 2500
+const ENLARGED_CANVAS_HEIGHT = 2500
+const BASE_RAMP = 0.0005;
+const USE_ISOMETRIC = true
 const config = {
     noiseSeed: NOISE_SEED,
     /** number of rows & columns */
@@ -71,8 +77,6 @@ const pos = {
     w: null,
     /** height */
     h: null,
-    /** size */
-    s: null,
     /** left */
     l: null,
     /** top */
@@ -98,8 +102,19 @@ let cells = [];
 let colorIndex = 0;
 
 function setupPosition() {
-    let w = window.innerWidth;
-    let h = window.innerHeight;
+    let cW = window.innerWidth;
+    let cH = window.innerHeight;
+
+    if (cW > cH) {
+        pos.cW = cH;
+        pos.cH = cH;
+    } else {
+        pos.cW = cW;
+        pos.cH = cW;
+    }
+
+    let w = ENLARGED_CANVAS_WIDTH;
+    let h = ENLARGED_CANVAS_WIDTH;
     if (w > h) {
         pos.w = h;
         pos.h = h;
@@ -107,10 +122,15 @@ function setupPosition() {
         pos.w = w;
         pos.h = w;
     }
-    pos.w = pos.w * CANVAS_WIDTH_MULTIPLIER;
-    pos.h = pos.h * CANVAS_HEIGHT_MULTIPLIER;
+    pos.w = w * CANVAS_WIDTH_MULTIPLIER;
+    pos.h = h * CANVAS_HEIGHT_MULTIPLIER;
     pos.t = 0;
     pos.l = 0;
+
+    if (DEBUG) {
+        console.log(pos);
+    }
+
 }
 
 function setupPallet() {
@@ -137,10 +157,10 @@ function setupSettings() {
     settings.rows = settings.cellsPerSide * CANVAS_HEIGHT_MULTIPLIER;
     settings.margin = setupMargin();
     settings.pallet = pallet;
-    settings.pixelDensityLow = 10
-    settings.pixelDensityHigh = 20
+    settings.pixelDensityLow = 20
+    settings.pixelDensityHigh = 30
     settings.pixelDensity = floor(map(fxrand(), 0, 1, settings.pixelDensityLow, settings.pixelDensityHigh));
-    settings.isoMaxMultiplier = floor(map(settings.pixelDensity, settings.pixelDensityLow, settings.pixelDensityHigh, 2, 5));
+    settings.isoMaxMultiplier = floor(map(settings.pixelDensity, settings.pixelDensityLow, settings.pixelDensityHigh, 5, 8));
     if (DEBUG) {
         console.log(settings);
     }
@@ -192,7 +212,7 @@ window.setup = () => {
     noiseSeed(config.noiseSeed);
     colorMode(RGB, 255, 255, 255, 1);
     init();
-    createCanvas(pos.w, pos.h);
+    createCanvas(pos.cW, pos.cH);
     pixelDensity(PX_DENSITY);
 };
 
@@ -204,66 +224,77 @@ function incrimentColorIndex() {
 }
 
 window.draw = () => {
-
-    // randomize the starting colorIndex
-    colorIndex = floor(map(fxrand(), 0, 1, 0, pallet.rgb.length));
+    noLoop();
     fill(255, 255, 255, 1);
     noStroke();
-    rect(0, 0, pos.w, pos.h)
-    noLoop();
+    rect(0, 0, pos.cW, pos.cH)
 
-    // for (const element of cells) {
-    //     let c = element;
-    //     stroke(0,0,0,1);
-    //     rect(c.x, c.y, c.w, c.h);
-    // }
-    angleMode(DEGREES);
-    push();
+
+    let cG = createGraphics(pos.w, pos.h);
+
+    cG.colorMode(RGB, 255, 255, 255, 1);
+    cG.pixelDensity(PX_DENSITY);
+    // randomize the starting colorIndex
+    colorIndex = floor(map(fxrand(), 0, 1, 0, pallet.rgb.length));
+    //cG.fill(255, 255, 255, 1);
+    cG.noStroke();
+    cG.rect(0, 0, pos.w, pos.h)
+
+    cG.angleMode(DEGREES);
+    cG.push();
     let roll = helper.rollADie(10);
+    roll = 1
     switch (roll) {
         case 5:
-            translate(pos.w, 0);
-            rotate(90)
+            cG.translate(pos.w, 0);
+            cG.rotate(90)
+            break;
+        case 6:
+            cG.translate(0, pos.h);
+            cG.rotate(-90)
             break;
         case 9:
-            translate(-pos.w / 1.79, pos.h / 2);
-            rotate(-45)
-            scale(1.5);
+            cG.translate(-pos.w / 1.783, pos.h / 2);
+            cG.rotate(-45)
+            cG.scale(1.5);
             break;
         case 10:
-            translate(pos.w / 2.05, -pos.h / 1.75);
-            rotate(45)
-            scale(1.5);
+            cG.translate(pos.w / 2.00, -pos.h / 1.783);
+            cG.rotate(45)
+            cG.scale(1.5);
             break;
         default:
 
     }
 
-    let horizon = createVector(0, pos.h / 2);
+    let horizon = cG.createVector(0, pos.h / 2);
     // get three colors used for gradient
-    let fistColorSet = [];
+    let colorSet = [];
     for (let i = 0; i < 5; i++) {
         let c1 = pallet.rgb[colorIndex];
         let color1 = color(c1[0], c1[1], c1[2], 1)
-        fistColorSet.push(color1)
+        colorSet.push(color1)
         incrimentColorIndex()
     }
 
+    let border = [];
     for (let i = 0; i < settings.columns / 2; i++) {
         let x = settings.cellWidth * i;
         let y = settings.cellHeight * i;
         let w = pos.w - ((settings.cellWidth * i) * 2);
         let h = dist(0, y, 0, horizon.y);
-        let ramp = 0.001
+        let ramp = BASE_RAMP;
         for (var t = 0; t < h; t++) {
             let tlerpPercent = t / h + ramp;
-            let cLerp = lerpColor(fistColorSet[0], fistColorSet[1], tlerpPercent);
-            noFill();
-            strokeWeight(1)
-            stroke(cLerp);
-            line(x, y + t, x + w, y + t);
-            ramp = sq(ramp);
+            let cLerp = lerpColor(colorSet[0], colorSet[1], tlerpPercent);
+            cG.noFill();
+            cG.strokeWeight(1.5)
+            cG.stroke(cLerp);
+            cG.line(x, y + t, x + w, y + t);
+            ramp+= BASE_RAMP
         }
+
+        border.push({"x": x, "y": y, "h": h, "w": w})
     }
 
     for (let i = 0; i < settings.columns / 2; i++) {
@@ -271,30 +302,62 @@ window.draw = () => {
         let y = settings.cellHeight * i + horizon.y - 1;
         let w = pos.w - ((settings.cellWidth * i) * 2);
         let h = dist(0, y, 0, pos.h);
-        let ramp = 0.001
+        let ramp = BASE_RAMP;
         for (let t = 0; t < h; t++) {
-            let tlerpPercent = t / h + ramp;
-            let cLerp = lerpColor(fistColorSet[1], fistColorSet[2], tlerpPercent);
-            noFill();
-            strokeWeight(1)
-            stroke(cLerp);
-            line(x, horizon.y + t, x + w, horizon.y + t);
-            ramp = sq(ramp);
+            let tlerpPercent = t / h - ramp;
+            let cLerp = cG.lerpColor(colorSet[1], colorSet[2], tlerpPercent);
+            cG.noFill();
+            cG.strokeWeight(1.5)
+            cG.stroke(cLerp);
+            cG.line(x, horizon.y + t, x + w, horizon.y + t);
+            ramp+= BASE_RAMP;
         }
     }
 
-    pop();
-    let gradients = get(0, 0, pos.w, pos.h);
-    let pixelPositions = [];
-    gradients.loadPixels();
-    for (var y = 0; y < pos.h; y += settings.pixelDensity) {
-        for (var x = 0; x < pos.w; x += settings.pixelDensity) {
+    for (var i = 0; i < border.length; i++) {
+      let b = border[i];
+      let c = colorSet[3];
+      c.setAlpha(0.05);
+      cG.stroke(c);
+      cG.strokeWeight(1);
+      cG.blendMode(MULTIPLY);
+      cG.beginShape();
+      cG.vertex(b.x, b.y + b.h);
+      cG.vertex(b.x, b.y);
+      cG.vertex(b.x + b.w, b.y);
+      cG.vertex(b.x + b.w, b.y + b.h);
+      cG.vertex(b.x + b.w, b.y + b.h * 2);
+      cG.vertex(b.x, b.y + b.h * 2);
+      cG.endShape(CLOSE);
+    }
 
-            let index = (x + y * width) * 4;
-            let r = gradients.pixels[index];
-            let g = gradients.pixels[index + 1];
-            let b = gradients.pixels[index + 2];
-            let a = gradients.pixels[index + 3];
+    let c = colorSet[3];
+    let lastBorder = border[border.length-1];
+    c.setAlpha(0.05);
+    cG.stroke(c);
+    cG.strokeWeight(1);
+    cG.blendMode(MULTIPLY);
+    cG.line(0, 0, lastBorder.x, lastBorder.y);
+    cG.line(pos.w, 0, lastBorder.x + lastBorder.w, lastBorder.y);
+    cG.line(pos.w, pos.h, lastBorder.x + lastBorder.w, lastBorder.y + lastBorder.h * 2);
+    cG.line(0, pos.h, lastBorder.x, lastBorder.y + lastBorder.h * 2)
+
+
+
+    cG.blendMode(BLEND);
+    cG.pop();
+    if(USE_ISOMETRIC) {
+
+    let pixelPositions = [];
+    cG.loadPixels();
+    for (var y = 0; y < cG.height; y += settings.pixelDensity) {
+        for (var x = 0; x < cG.width; x += settings.pixelDensity) {
+
+            let index = (x + y * cG.width) * 4;
+            let r = cG.pixels[index];
+            let g = cG.pixels[index + 1];
+            let b = cG.pixels[index + 2];
+            let a = cG.pixels[index + 3];
             let averageColor = helper.mean([r, g, b]);
             pixelPositions.push(
                 {
@@ -309,14 +372,18 @@ window.draw = () => {
         }
 
     }
-    let rollIndex = 1;
-    for (const element of pixelPositions) {
-        let p = element;
+    cG.updatePixels();
+
+    for (var i = 0; i < pixelPositions.length; i++) {
+
+        let p = pixelPositions[i];
+
         let isosSideWidth = map(p.average, 0, 100, 1, settings.isoMaxMultiplier);
         let isosSizeMultipler = map(p.average, 0, 255, 1, settings.isoMaxMultiplier);
-        let isosLengthMultipler = map(p.average, 0, 255, 1, settings.isoMaxMultiplier);
+        let isoLengthMultipler = map(p.average, 0, 255, 1, settings.isoMaxMultiplier);
 
-        noStroke();
+
+        cG.noStroke();
         let iso = new Isometric(
             p.position.x,
             p.position.y,
@@ -325,42 +392,58 @@ window.draw = () => {
             isosSizeMultipler
         );
 
-        iso.pickDirection(helper.rollADie(rollIndex), isosLengthMultipler);
+        iso.pickDirection(ceil(map(fxrand(), 0, 1, 1, 7)), isoLengthMultipler);
 
         if (vectorsAreInsideBounds(iso)) {
-            fill(
+
+            cG.fill(
                 p.red * settings.faceLightness.top,
                 p.green * settings.faceLightness.top,
                 p.blue * settings.faceLightness.top,
                 1
             );
-            iso.buildTopFace();
+            // build the top
+            cG.beginShape();
+            cG.vertex(iso.g.x, iso.g.y);
+            cG.vertex(iso.c.x, iso.c.y);
+            cG.vertex(iso.d.x, iso.d.y);
+            cG.vertex(iso.e.x, iso.e.y);
+            cG.endShape(CLOSE)
 
-            fill(
+            cG.fill(
                 p.red * settings.faceLightness.left,
                 p.green * settings.faceLightness.left,
                 p.blue * settings.faceLightness.left,
                 1
             );
-            iso.buildLeftFace();
+            // iso.buildLeftFace();
+            cG.beginShape();
+            cG.vertex(iso.a.x, iso.a.y);
+            cG.vertex(iso.b.x, iso.b.y);
+            cG.vertex(iso.c.x, iso.c.y);
+            cG.vertex(iso.g.x, iso.g.y);
+            cG.endShape(CLOSE)
 
-            fill(
+            cG.fill(
                 p.red * settings.faceLightness.right,
                 p.green * settings.faceLightness.right,
                 p.blue * settings.faceLightness.right,
                 1
             );
-            iso.buildRightFace();
-        }
-
-        if (rollIndex === 7) {
-          rollIndex = 1
-        } else {
-          rollIndex++;
+            //iso.buildRightFace();
+            cG.beginShape();
+            cG.vertex(iso.a.x, iso.a.y);
+            cG.vertex(iso.g.x, iso.g.y);
+            cG.vertex(iso.e.x, iso.e.y);
+            cG.vertex(iso.f.x, iso.f.y);
+            cG.endShape(CLOSE)
         }
 
     }
 
+  }
+
+  image(cG,0,0,pos.cW,pos.cH);
 
 }
 let saveCount = 0;
